79330fd4ed9928ce60ee473148116ec6e84ed489
Tom Lorius <tomlorius.mail@gmail.com>
2026-03-28 08:23:47 +0100

    owo

--- vitest.config.ts
+++ vitest.config.ts
@@ -10,18 +10,6 @@ export default defineConfig({
     setupFiles: [],
     include: ["src/**/*.test.ts", "src/**/*.test.tsx"],
     exclude: ["node_modules", "dist", ".idea", ".git", ".cache"],
-    coverage: {
-      provider: "v8",
-      reporter: ["text", "json", "html"],
-      include: ["src/game/**/*.ts", "src/game/**/*.tsx"],
-      exclude: ["node_modules/", "dist/", "**/*.d.ts", "**/*.config.ts"],
-      thresholds: {
-        lines: 80,
-        statements: 80,
-        functions: 78,
-        branches: 63,
-      },
-    },
   },
   resolve: {
     alias: {
